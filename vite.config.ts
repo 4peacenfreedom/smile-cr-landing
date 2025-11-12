76a0868f29198bd52313ed06580c5d28158b4ac7
David Romero <4peacenfreedom@gmail.com>
2025-11-11 18:44:25 -0600

    configurar GitHub Pages y renombrar base path

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: mode === "production" ? "/dentalcare/" : "/", // Adjust base path for production
 }));
